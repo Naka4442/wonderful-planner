fbc1ff0c57eb3525adab19bc23106eb732c5d8e5
toyfigure <aroslavsolanoj24@gmail.com>
2026-01-06 19:44:03 +0300

    Сервис регистрации

---
+++ services/user_services.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+from models import User
+
+
+class UserServices:
+    def __init__(self, db: Session ):
+        self.db = db
+
+    def signup(self, name: str, email: str, password: str, password2: str):
+        same_email = self.db.query(User).filter(User.email == email).first()
+        if same_email:
+            raise ValueError("Пользователь с таким email уже существует")
+        if password != password2:
+            raise ValueError("Пароли не совпадают")
+
+        user = User(name=name, email=email, password=password)
+        self.db.add(user)
+        self.db.commit()
