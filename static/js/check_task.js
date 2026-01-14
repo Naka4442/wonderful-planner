9f2db6e16199c66ab8e69366c9bc23e1415756ea
Alex Chernov <nak4442@gmail.com>
2026-01-14 20:45:12 +0300

    Начало окна с завершением задачи

--- static/js/check_task.js
+++ static/js/check_task.js
@@ -1,9 +1,16 @@
 // Взяли все чекбоксы
 const checkboxes = Array.from(document.querySelectorAll(".check"));
+const modal = document.querySelector(".modal");
+
 
 checkboxes.forEach(checkbox => {
     checkbox.addEventListener("input", () => {
         const taskId = Number(checkbox.parentElement.querySelector(".task-id").value);
         console.log(`Вы нажали на чекбокс ${taskId}`);
+        modal.classList.remove("hidden");
     })
+})
+
+modal.addEventListener("click", () => {
+    modal.classList.add("hidden");
 })
