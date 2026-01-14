3c3fd40d3e2767d404edf937789a876e9509909d
Alex Chernov <nak4442@gmail.com>
2026-01-14 21:33:16 +0300

    Получение актуального времени задачи

--- static/js/check_task.js
+++ static/js/check_task.js
@@ -1,16 +1,32 @@
 // Взяли все чекбоксы
 const checkboxes = Array.from(document.querySelectorAll(".check"));
 const modal = document.querySelector(".modal");
+const taskTitleElement = document.querySelector(".check-task-modal-title");
 
+const taskHoursElement = document.querySelector("#time-hours");
+const taskMinutesElement = document.querySelector("#time-minutes");
+const taskCheckButton = document.querySelector(".check-task-modal-button");
+
+let activeTaskId;
 
 checkboxes.forEach(checkbox => {
-    checkbox.addEventListener("input", () => {
-        const taskId = Number(checkbox.parentElement.querySelector(".task-id").value);
-        console.log(`Вы нажали на чекбокс ${taskId}`);
+    checkbox.addEventListener("input", (e) => {
+        e.target.checked = false;
+        activeTaskId = Number(checkbox.parentElement.querySelector(".task-id").value);
+        const taskTitle = checkbox.parentElement.querySelector("h4").textContent;
         modal.classList.remove("hidden");
+        taskTitleElement.textContent = taskTitle;
     })
 })
 
-modal.addEventListener("click", () => {
+modal.addEventListener("click", (e) => {
+    if(e.target === modal){
+        modal.classList.add("hidden");
+    }
+})
+
+taskCheckButton.addEventListener("click", (e) => {
+    const taskTime = Number(taskHoursElement.value) * 60 + Number(taskMinutesElement.value);
+    console.log(activeTaskId, taskTime);
     modal.classList.add("hidden");
 })
