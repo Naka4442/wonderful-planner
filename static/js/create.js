40aff9b020cb4584fab2000f4893fef34cbdf152
toyfigure <aroslavsolanoj24@gmail.com>
2026-01-25 16:55:54 +0300

    новое создание задач

--- static/js/create.js
+++ static/js/create.js
@@ -1,6 +1,9 @@
 const isRepeatedCheckbox = document.querySelector("#is_repeated");
 const repeatedBlock = document.querySelector(".repeated-block");
 const notRepeatedBlock = document.querySelector(".not-repeated-block");
+const EventRadio = document.querySelectorAll('input[name="event"]');
+const eventBlock = document.querySelector(".event-block");
+const taskBlock = document.querySelector(".task-block");
 
 
 isRepeatedCheckbox.addEventListener("input", (e) => {
@@ -11,4 +14,19 @@ isRepeatedCheckbox.addEventListener("input", (e) => {
         notRepeatedBlock.classList.remove("hidden");
         repeatedBlock.classList.add("hidden");
     }
+})
+
+EventRadio.forEach(radio => {
+    radio.addEventListener("input", (e) => {
+        const value = Number(e.target.value);
+        if(value === 1){
+        eventBlock.classList.remove("hidden");
+        taskBlock.classList.add("hidden");
+        }
+        else {
+        taskBlock.classList.remove("hidden");
+        eventBlock.classList.add("hidden");
+        }
+
+    })
 })
